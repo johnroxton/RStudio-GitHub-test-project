b4bb2e4e8381372bb489698f2cab3728f0b8bf2e
David Schellenberger Costa <david.schellenberger.costa@uni-leipzig,de>
2026-01-20 14:20:00 +0100

    first test

---
+++ test code.R
@@ -0,0 +1,20 @@
+# test GitHub
+
+# load in libraries
+library(data.table)
+
+# do some calc
+a = 1
+b = 2
+a + b
+
+# create some other files
+pdf("test figure 1.pdf")
+  plot(1:10, pch = 2, col = 1:10, cex = 2, lwd=5)
+dev.off()
+
+# create a test file
+testData <- data.table(matrix(1:100, 10, 10))
+fwrite(testData, file = "test file 1.txt")
+fwrite(testData, file = "test file 2.csv")
+fwrite(testData, file = "test file 3.gz")
